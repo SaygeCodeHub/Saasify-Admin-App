7d75840b39c90bcb156e812d57ac018b52a486a9
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2024-03-29 10:14:00 +0530

    saas-rucha: Refactor retrieve image from firebase file.

--- lib/utils/retrieve_image_from_firebase.dart
+++ lib/utils/retrieve_image_from_firebase.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class RetrieveImageFromFirebase {
   Future<String> getImage(String imagePath) async {
     String image = '';
+    User? user = FirebaseAuth.instance.currentUser;
     final storageRef = FirebaseStorage.instance.ref();
-    final imagesRef = storageRef.child("user_images/");
+    final imagesRef = storageRef.child("user_images/${user?.uid}");
     final uploadTask = imagesRef.putFile(File(imagePath));
     final completedTask = await uploadTask;
     final downloadUrl = await completedTask.ref.getDownloadURL();

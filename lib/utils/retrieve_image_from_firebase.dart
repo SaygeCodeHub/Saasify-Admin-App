8f7eaa85a119d52de719c0db6897ce14d1dc0a72
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2024-04-04 14:29:40 +0530

    SAAS-rucha: Reformat code.

--- lib/utils/retrieve_image_from_firebase.dart
+++ lib/utils/retrieve_image_from_firebase.dart
@@ -8,18 +8,27 @@ class RetrieveImageFromFirebase {
   Future<String> getImage(String imagePath) async {
     String image = '';
     if (imagePath.isNotEmpty) {
-      User? user = FirebaseAuth.instance.currentUser;
+      final User? user = FirebaseAuth.instance.currentUser;
       final storageRef = FirebaseStorage.instance.ref();
       final imagesRef = storageRef.child("user_images/${user?.uid}/");
-      final uploadTask = (kIsWeb)
-          ? imagesRef.putString(imagePath)
-          : imagesRef.putFile(File(imagePath));
-      final completedTask = await uploadTask;
-      final downloadUrl = await completedTask.ref.getDownloadURL();
-      image = downloadUrl;
-      return image;
-    } else {
-      return '';
+
+      final fileName = imagePath.split('/').last; // Extract filename
+
+      try {
+        print('inside try----');
+        final ref = imagesRef.child(fileName);
+        final downloadUrl = await ref.getDownloadURL(); // Check if file exists
+        image = downloadUrl; // Use existing download URL
+      } catch (e) {
+        print('inside catch----');
+        final uploadTask = (kIsWeb)
+            ? imagesRef.putString(imagePath)
+            : imagesRef.putFile(File(imagePath));
+        final completedTask = await uploadTask;
+        final downloadUrl = await completedTask.ref.getDownloadURL();
+        image = downloadUrl;
+      }
     }
+    return image;
   }
 }
